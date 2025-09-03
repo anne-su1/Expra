f69d7168e43bd53de624bf237812e8a79a43ad79
Anne <tildaapfelkern16@gmail.com>
2025-09-03 16:36:42 +0200

    added second data frame

--- Replikation_Hu.py
+++ Replikation_Hu.py
@@ -5,11 +5,6 @@ from psychopy import core, visual, gui, event   # core: basisfkt (timing), visua
 import random                                   # ähnlich numpy.random.shuffle
 import time                                     # kann, muss aber nicht verwendet werden
 
-# easy input
-sub_id = '01'               # white-format
-age = 23
-sex = 'm'
-glasses = True
 
 supervisor_input = gui.Dlg(title="Participant Data", pos=(0, 0))
 supervisor_input.addField("sub_id")
@@ -18,6 +13,35 @@ supervisor_input.addField("sex", choices=["male", "female"])
 supervisor_input.addField("normal or corrected sight", choices=["yes", "no"])
 supervisor_input.addField("handedness", choices=["right", "left"])
 supervisor_input.show()
+
+# create a folder and a dataframe to store the output
+output_path = os.getcwd() + f'/sub-{sub_id}'
+if not os.path.exists(output_path):         # wenn Pfad nicht existiert, mache diesen ordner (makedirs)
+    os.makedirs(output_path)                # zweites if Statement nötig, wenn pfad nicht existiert, um nicht weiter um nicht zu überschreiben
+    
+behav_data = pd.DataFrame({'sub_id' : [],               # dictionary nur dann verwenden bzw verändern wenn wirklich nötig 
+                            'age' : [],                 # [] leere Liste
+                            'sex' : [],                 # dataframe wird mit jedem versuchsdurchgang gefüllt
+                            'normal or corrected sight' : [],
+                            'handedness' : [],
+                            'block' : [],
+                            'trial' : [],
+                            'reaction_time' : [],
+                            'key_pressed' : []
+                            })
+
+quest_data = pd.DataFrame({'sub_id' : [],               # dictionary nur dann verwenden bzw verändern wenn wirklich nötig 
+                            'Q1E1' : [],                 # [] leere Liste
+                            'Q1E2' : [],                 # dataframe wird mit jedem versuchsdurchgang gefüllt
+                            'Q1E3' : [],
+                            'Q1E4' : [],
+                            'Q1E5' : [],
+                            'Q1E6' : [],
+                            'Q2Phase1' : [],
+                            'Q2Phase2' : []
+                            })
+        
+file_path = output_path + f'/sub-{sub_id}_task-catshumans.tsv' # TO DO: change task_catsHuman to real name
 
 # window specific to our hardware
 win = visual.Window(                    # Psychopy benutzt in visual, window als fkt von visual
